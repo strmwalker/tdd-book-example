5fc10e84488516c7d5235fed7ccd96254c29684b
yurathestorm <yurathestorm@gmail.com>
2018-05-21 13:50:37 +0300

    End of first chapter.
    Refactoring assert statements into Unittest.TestCases, and moving on to the second chapter.

--- functional_tests.py
+++ functional_tests.py
@@ -1,12 +1,26 @@
+import unittest
+
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 
+
+class HomePageTest(unittest.TestCase):
+
+    def setUp(self):
+        options = Options()
+        options.add_argument('-headless')
+        self.browser = Firefox(executable_path=r'C:\Users\yurblago\Documents\tdd-intro\geckodriver\geckodriver.exe',
+                               options=options,
+                               firefox_binary=r"C:\Users\yurblago\AppData\Local\Mozilla Firefox\firefox.exe"
+                               )
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_home_page(self):
+        self.browser.get('http:localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+
+
 if __name__ == '__main__':
-    options = Options()
-    options.add_argument('-headless')
-    browser = Firefox(executable_path=r'C:\Users\yurblago\Documents\tdd-intro\geckodriver\geckodriver.exe',
-                      firefox_options=options,
-                      firefox_binary=r"C:\Users\yurblago\AppData\Local\Mozilla Firefox\firefox.exe"
-                      )
-    browser.get('http:localhost:8000')
-    assert 'Django' in browser.title
+    unittest.main()
