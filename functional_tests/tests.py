2db2c09586467bd4f3d4199ab182dc15b4cee8ae
yurathestorm <yurathestorm@gmail.com>
2018-05-23 12:53:29 +0300

    Chapter 7
    New url, view and template to display lists.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,17 +6,21 @@ from selenium.webdriver import Firefox
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 
-MAX_WAIT = 10
+MAX_WAIT = 5
+
 
 class NewVisitorTest(LiveServerTestCase):
 
-    def setUp(self):
+    def get_browser(self):
         options = Options()
         options.add_argument('-headless')
         self.browser = Firefox(executable_path=r'C:\Users\yurblago\Documents\tdd-intro\geckodriver\geckodriver.exe',
                                options=options,
                                firefox_binary=r"C:\Users\yurblago\AppData\Local\Mozilla Firefox\firefox.exe"
                                )
+
+    def setUp(self):
+        self.get_browser()
 
     def tearDown(self):
         self.browser.quit()
@@ -66,16 +70,49 @@ class NewVisitorTest(LiveServerTestCase):
         # The page updates again, and now shows both items on her list
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-        self.fail('Finish the test')
-
-        # She visits that URL - her to-do list is still there.
-
         # Satisfied, she goes back to sleep
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         self.browser.get(self.live_server_url)
-        input_box =
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Buy peacock feathers')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # Now a new user, Francis comes along to the site.
+
+        """
+        We use a new browser session to make sure that no information
+        of Edith's is coming through from cookies etc
+        """
+        self.browser.quit()
+        self.get_browser()
+
+        # Francis visits the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a new list by entering a new item. He is less interesting
+        # than Edith ...
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Buy milk')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there's no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
--- superlists/urls.py
+++ superlists/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 
-from lists.views import home_page
+from lists.views import home_page, view_list, new_list
 
 urlpatterns = [
-    url(r'^$', home_page),
+    url(r'^$', home_page, name='home'),
+    url(r'^lists/new$', new_list, name='new_list'),
+    url(r'^lists/the-only-list-in-the-world/$', view_list, name='view_list'),
 ]
