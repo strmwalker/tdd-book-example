ee2e5fe60856b1a2828869dbd9877442a356aea6
yurathestorm <yurathestorm@gmail.com>
2018-06-18 12:39:26 +0300

    ALLOWED_HOSTS

--- superlists/settings.py
+++ superlists/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-    ALLOWED_HOSTS = [os.environ['SITENAME']]
+    ALLOWED_HOSTS = [os.environ['SITENAME'],]
 else:
     DEBUG = True
     SECRET_KEY = 'insecure-key-for-dev'
