2b8be10cf5227689b52b19a88537aa6fb48fb151
yurathestorm <yurathestorm@gmail.com>
2018-06-22 18:00:32 +0300

    Preventing duplicates at model layer
    Signed-off-by: yurathestorm <yurathestorm@gmail.com>

--- lists/models.py
+++ lists/models.py
@@ -13,4 +13,6 @@ class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None, on_delete=models.DO_NOTHING)
 
+    class Meta:
+        unique_together = ('list', 'text')
 
