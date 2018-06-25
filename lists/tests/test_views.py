9e798e2e6414eac11830c952a4a7bb0383c7572b
yurathestorm <yurathestorm@gmail.com>
2018-06-25 12:45:02 +0300

    Experimenting with duplicate Item Validation at the Views layer. Tests doesn't work, server shows proper IntegrityErrors.
    Signed-off-by: yurathestorm <yurathestorm@gmail.com>

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -99,6 +99,21 @@ class ListViewTest(TestCase):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item = Item.objects.create(list=list1, text='textey')
+
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'test': 'textey'}
+        )
+
+        expected_error = escape("You've already got this in your list")
+        # print('\n'.join(response.content.decode('utf-8').split('\n')))
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
+
 
 class NewListTest(TestCase):
 
